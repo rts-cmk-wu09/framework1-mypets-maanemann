fb56c0d422d7016f738a7b68cb84331aead8a9dc
maanemann <hans.august@gmail.com>
2023-05-31 12:27:28 +0200

    Add færdige prikker

--- my_pet/src/templates/Dialog.jsx
+++ my_pet/src/templates/Dialog.jsx
@@ -11,13 +11,13 @@ const StyledBut = styled.button`
   margin: 106px auto 46px auto;
   width: 100%;
   height: 44px;
-  border-radius: 100px;
+  border-radius: 10rem;
   background-color: #57419d;
   color: white;
   /* font-weight: 700; */
 `;
 const PageDots = styled.div`
-  margin: 0 auto;
+  margin: 2rem auto 0 auto;
   width: fit-content;
   display: flex;
   gap: 3px;
@@ -25,8 +25,15 @@ const PageDots = styled.div`
 const InactDot = styled.div`
   width: 3px;
   height: 3px;
-  background-color: gray;
+  background-color: rgba(85, 51, 234, 0.21);
+  border-radius: 100px;
 `;
+const ActDot = styled.div`
+  width: 18px;
+  height: 3px;
+  background-color: rgb(85, 51, 234);
+  border-radius: 100px;
+`
 
 const Dialog = () => {
   return (
@@ -35,6 +42,7 @@ const Dialog = () => {
       <PageDots>
         <InactDot></InactDot>
         <InactDot></InactDot>
+        <ActDot></ActDot>
       </PageDots>
       <StyledBut>Skip</StyledBut>
     </StyledDiv>
