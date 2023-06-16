ef5098891b0f1201bb84b237268566a8d1d1c2d2
maanemann <hans.august@gmail.com>
2023-06-16 13:31:42 +0200

    Add klokke i notification ikonet

--- my_pet/src/components/Notification.jsx
+++ my_pet/src/components/Notification.jsx
@@ -3,11 +3,21 @@ import "../css/popBut.css";
 import "../css/popBut__sq-la.css";
 
 const StyledDiv = styled.div`
-    justify-self: end;
-`
+  justify-self: end;
+  display: grid;
+  & > i {
+    display: block;
+    margin: auto;
+    color: var(--bright-accent);
+  }
+`;
 
 const Notification = () => {
-    return ( <StyledDiv className="popBut popBut__sq-la"></StyledDiv> );
-}
+  return (
+    <StyledDiv className="popBut popBut__sq-la">
+      <i className={"fa-regular fa-bell"}></i>
+    </StyledDiv>
+  );
+};
 
 export default Notification;
